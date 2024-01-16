1c97616f81ccd91389da5070a392b31bd02edecc
johnnylozano <102393958+johnnylozano@users.noreply.github.com>
2024-01-15 20:08:00 -0500

    feat: changed title and added github link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { useSidebarToggle } from "@/context";
-import { GearIcon, HamburgerMenuIcon } from "@radix-ui/react-icons";
+import {
+  GearIcon,
+  GitHubLogoIcon,
+  HamburgerMenuIcon,
+} from "@radix-ui/react-icons";
 import { Orbitron } from "next/font/google";
 import {
   Sheet,
@@ -10,6 +14,13 @@ import {
   SheetTrigger,
   SheetTitle,
 } from "./ui/sheet";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+import Link from "next/link";
 
 const orbitron = Orbitron({ subsets: ["latin"] });
 
@@ -31,35 +42,51 @@ function Navbar() {
           Music Player
         </p>
       </div>
-      <Sheet>
-        <SheetTrigger asChild>
-          <button className="rounded-full p-2 hover:bg-white/25 transition-all duration-150">
-            <GearIcon className="h-5 w-5 text-gray-200" />
-          </button>
-        </SheetTrigger>
-        {/* @ts-expect-error */}
-        <SheetContent className="bg-zinc-700/95">
-          <SheetHeader>
-            <SheetTitle className="text-gray-200">
-              Keyboard Shortcuts
-            </SheetTitle>
-            <div className="p-2 rounded-lg bg-zinc-900/65 shadow-inner shadow-black/70">
-              <span className="mt-4 text-gray-300 text-sm font-medium flex justify-between">
-                Toggle Play/Pause
-                <kbd className="bg-zinc-800 border shadow shadow-gray-200/10 border-white/25 px-1.5 rounded flex-none">
-                  Ctrl + Space
-                </kbd>
-              </span>
-              <span className="mt-4 text-gray-300 text-sm font-medium flex justify-between">
-                Toggle Mute
-                <kbd className="bg-zinc-800 border shadow shadow-gray-200/10 border-white/25 px-1.5 rounded flex-none">
-                  Ctrl + M
-                </kbd>
-              </span>
-            </div>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
+      <div className="flex gap-2 items-center">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link
+                href="https://github.com/johnnylozano/music-player"
+                target="_blank"
+                className="rounded-full p-2 hover:bg-white/25 transition-all duration-150"
+              >
+                <GitHubLogoIcon className="h-5 w-5 text-white" />
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent>Source Code</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <Sheet>
+          <SheetTrigger asChild>
+            <button className="rounded-full p-2 hover:bg-white/25 transition-all duration-150">
+              <GearIcon className="h-5 w-5 text-gray-200" />
+            </button>
+          </SheetTrigger>
+          {/* @ts-expect-error */}
+          <SheetContent className="bg-zinc-700/95">
+            <SheetHeader>
+              <SheetTitle className="text-gray-200">
+                Keyboard Shortcuts
+              </SheetTitle>
+              <div className="p-2 rounded-lg bg-zinc-900/65 shadow-inner shadow-black/70">
+                <span className="mt-4 text-gray-300 text-sm font-medium flex justify-between">
+                  Toggle Play/Pause
+                  <kbd className="bg-zinc-800 border shadow shadow-gray-200/10 border-white/25 px-1.5 rounded flex-none">
+                    Ctrl + Space
+                  </kbd>
+                </span>
+                <span className="mt-4 text-gray-300 text-sm font-medium flex justify-between">
+                  Toggle Mute
+                  <kbd className="bg-zinc-800 border shadow shadow-gray-200/10 border-white/25 px-1.5 rounded flex-none">
+                    Ctrl + M
+                  </kbd>
+                </span>
+              </div>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
     </header>
   );
 }
